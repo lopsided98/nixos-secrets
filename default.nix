64e4e88f3345f0cdc5fc6d7bc56bc955b3ec4da4
Ben Wolsieffer <benwolsieffer@gmail.com>
2022-01-02 12:02:57 -0500

    Use gnupg minimal

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ lib, nixShell ? false, buildPythonApplication, mypy, python-gnupg }:
+{ lib, nixShell ? false, buildPythonApplication, mypy, python-gnupg, gnupg }:
 
 buildPythonApplication {
   name = "nixos-secrets";
@@ -16,7 +16,24 @@ buildPythonApplication {
   };
 
   nativeBuildInputs = [ mypy ];
-  propagatedBuildInputs = [ python-gnupg ];
+  propagatedBuildInputs = [
+    (python-gnupg.override {
+      gnupg = gnupg.override {
+        enableMinimal = true;
+        guiSupport = false;
+        pcsclite = null;
+        sqlite = null;
+        pinentry = null;
+        adns = null;
+        gnutls = null;
+        libusb1 = null;
+        openldap = null;
+        readline = null;
+        zlib = null;
+        bzip2 = null;
+      };
+    })
+  ];
 
   meta = with lib; {
     description = "Encrypted secrets management for NixOS";
